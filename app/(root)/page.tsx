b7273f3a75c6b7854f8a461991b6e1004c3056e9
mhmd-aashik <aashikdevelop@gmail.com>
2024-04-19 18:55:43 +0530

    allusers

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,8 +3,12 @@ import LogoSlider from "@/components/shared/home/LogoSlider";
 import Review from "@/components/shared/home/Review";
 import ShuffleHero from "@/components/shared/home/ShuffleHero";
 import VideoSction from "@/components/shared/home/VideoSction";
+import { getAllUsers } from "@/lib/actions/user.actions";
 
-export default function Home() {
+export default async function Home() {
+  const allUsers = await getAllUsers();
+  console.log(allUsers);
+
   return (
     <main className="bg-[#020617]">
       <ShuffleHero />
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -8,6 +8,17 @@ import User from "@/models/userModel";
 import Event from "@/models/eventModel";
 import Order from "@/models/order.model";
 
+export async function getAllUsers() {
+  try {
+    await connectToDatabase();
+
+    const alluser = await User.find({});
+    return JSON.parse(JSON.stringify(alluser));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 export async function createUser(user: CreateUserParams) {
   try {
     await connectToDatabase();
