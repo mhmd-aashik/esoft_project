49defe611cee8ac9a4a82c542f5255ba96bdcc4c
mhmd-aashik <aashikdevelop@gmail.com>
2024-04-20 09:41:47 +0530

    ijlal told me to puct the changes

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,12 +1,95 @@
-import Image from "next/image";
 import React from "react";
+import { getEventById } from "@/lib/actions/events.action";
+import { SearchParamProps } from "@/types";
+import Image from "next/image";
+import { formatDateTime } from "@/lib/utils";
 
-const page = () => {
+const EventDetails = async ({ params: { id } }: SearchParamProps) => {
+  const event = await getEventById(id);
+  console.log(event);
+
   return (
-    <div className="flex items-center justify-center">
-      <Image src="/assets/qwe.jpeg" width={700} height={700} alt="image" />
-    </div>
+    <section className="flex justify-center bg-[#020617] bg-dotted-pattern bg-contain">
+      <div className="grid grid-cols-1 lg:grid-cols-2 2xl:max-w-7xl">
+        <Image
+          src={event?.imageUrl}
+          alt="event"
+          width={1000}
+          height={1000}
+          className="h-full min-h-[300px] object-cover object-center"
+        />
+
+        <div className="flex w-full flex-col gap-8 p-5 md:p-10">
+          <div className="flex flex-col gap-6">
+            <h2 className="h2-bold text-slate-500">{event?.title}</h2>
+            <div className="flex flex-col gap-3 lg:flex-row lg:items-center">
+              <div className="flex gap-3">
+                <p className="p-bold-20 rounded-full bg-green-500/10 px-5 py-2 text-green-700 text-center">
+                  {event.category}
+                </p>
+
+                <p className="p-medium-16 rounded-full bg-blue-800 px-4 py-2.5 text-gray-500 text-center ">
+                  {`Age : ${event.age}`}
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-5">
+            <div className="flex gap-2 md:gap-3">
+              <Image
+                src="/assets/icons/calendar.svg"
+                width={32}
+                height={32}
+                alt="calendar"
+              />
+              <div className="p-medium-16 lg:p-regular-20 flex flex-col flex-wrap items-center text-white   ">
+                <p>
+                  {formatDateTime(event.startDateTime).dateOnly} -{" "}
+                  {formatDateTime(event.startDateTime).timeOnly}
+                </p>
+                <p>
+                  {formatDateTime(event.endDateTime).dateOnly} -{" "}
+                  {formatDateTime(event.endDateTime).timeOnly}
+                </p>
+              </div>
+            </div>
+            <div className="flex items-center justify-start">
+              <Image
+                src={event?.organizer.photo}
+                alt="organizer"
+                width={32}
+                height={32}
+                className="rounded-full md:flex hidden"
+              />
+              <p className="p-medium-18 ml-2 mt-2 sm:mt-0">
+                by{"  "}{" "}
+                <span className="text-primary-500">
+                  {event.organizer.firstName} {event.organizer.lastName}
+                </span>
+              </p>
+            </div>
+            <div className="p-regular-20 flex items-center gap-3">
+              <Image
+                src="/assets/icons/location.svg"
+                width={32}
+                height={32}
+                alt="location"
+              />
+              <p className="p-medium-16 lg:p-regular-20">{event.location}</p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-2">
+            <p className="p-bold-20 text-white">What you will learn</p>
+            <p className="p-medium-16 lg:p-regular-18 text-yellow-100">{event.description}</p>
+            <p className="p-medium-16 lg:p-regular-18 truncate text-primary-500 underline">
+              {event.url}
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
-export default page;
+export default EventDetails;
--- lib/actions/events.action.ts
+++ lib/actions/events.action.ts
@@ -1,29 +1,58 @@
 "use server";
 
-import { CreateEventParams } from "@/types";
-import { connectToDatabase } from "../mongoose";
-import User from "@/models/userModel";
-import Event from "@/models/eventModel";
+import { revalidatePath } from "next/cache";
 
-export const createEvent = async ({
-  event,
-  userId,
-  path,
-}: CreateEventParams) => {
+import {
+  CreateEventParams,
+  UpdateEventParams,
+  DeleteEventParams,
+  GetAllEventsParams,
+  GetEventsByUserParams,
+  GetRelatedEventsByCategoryParams,
+} from "@/types";
+import { connectToDatabase } from "../mongoose";
+import Event from "@/models/eventModel";
+import User from "@/models/userModel";
+
+const populateEvent = (query: any) => {
+  return query.populate({
+    path: "organizer",
+    model: User,
+    select: "_id firstName lastName photo",
+  });
+};
+
+// CREATE
+export async function createEvent({ userId, event, path }: CreateEventParams) {
   try {
     await connectToDatabase();
 
     const organizer = await User.findById(userId);
-
     if (!organizer) throw new Error("Organizer not found");
 
     const newEvent = await Event.create({
       ...event,
       organizer: userId,
     });
+    revalidatePath(path);
 
     return JSON.parse(JSON.stringify(newEvent));
   } catch (error) {
     console.log(error);
   }
-};
+}
+
+// GET ONE EVENT BY ID
+export async function getEventById(eventId: string) {
+  try {
+    await connectToDatabase();
+
+    const event = await populateEvent(Event.findById(eventId));
+
+    if (!event) throw new Error("Event not found");
+
+    return JSON.parse(JSON.stringify(event));
+  } catch (error) {
+    console.log(error);
+  }
+}
