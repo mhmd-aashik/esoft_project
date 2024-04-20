614378a49f3045edb8ddee580f6e2c446e8b8a0e
mhmd-aashik <aashikdevelop@gmail.com>
2024-04-20 08:16:24 +0530

    donnnn

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const page = () => {
   return (
-    <div>Wel cm are you a esoft student</div>
-  )
-}
+    <div className="flex items-center justify-center">
+      <Image
+        src="https://st3.depositphotos.com/4196725/13795/i/1600/depositphotos_137951874-stock-photo-young-funny-man-with-angry.jpg"
+        width={700}
+        height={700}
+        alt="image"
+      />
+    </div>
+  );
+};
 
-export default page
+export default page;
