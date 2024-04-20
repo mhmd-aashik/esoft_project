df0f603b2f2b1954ec4ea37c547ea190b3242b3a
mhmd-aashik <aashikdevelop@gmail.com>
2024-04-20 08:23:40 +0530

    image

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -4,12 +4,7 @@ import React from "react";
 const page = () => {
   return (
     <div className="flex items-center justify-center">
-      <Image
-        src="https://st3.depositphotos.com/4196725/13795/i/1600/depositphotos_137951874-stock-photo-young-funny-man-with-angry.jpg"
-        width={700}
-        height={700}
-        alt="image"
-      />
+      <Image src="/assets/qwe.jpeg" width={700} height={700} alt="image" />
     </div>
   );
 };
